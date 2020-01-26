0fdb4135f1a85056e47ddfa9c11ab683beb74451
Aurélien Girault <me@agirault.com>
2020-01-26 10:26:43 +0100

    Initial Commit

---
+++ SwiftUIWarCardGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIWarCardGame
+//
+//  Created by Aurélien Girault on 26/01/2020.
+//  Copyright © 2020 Aurélien Girault. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
